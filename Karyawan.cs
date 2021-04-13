305dca57a95c453e60d5e1a95bacb6a0281f5a0a
hasifpriyambudi <hasifpriyambudi13@gmail.com>
2021-04-13 11:34:11 +0700

    First Commit

---
+++ Karyawan.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ResponsiPemrograman3305
+{
+    public class Karyawan{
+        public string Nik {get;set;}
+        public string Nama {get;set;}
+        public int GajiBulanan {get;set;}
+        
+        public void infoKaryawan(){
+            Console.WriteLine("No NIK/Nama              Gaji Bulanan");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("1. {0}/{1}             Rp.{2:#,0}",Nik,Nama,GajiBulanan);
+        }
+
+        public void naikGaji(){
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Alhamdulilah dapat kenaikan gaji 10%");
+            int naik = (GajiBulanan/100)*10;
+            int hasilAkhir = naik+GajiBulanan;
+            Console.WriteLine("No NIK/Nama              Gaji Bulanan");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("1. {0}/{1}             Rp.{2:#,0}",Nik,Nama,hasilAkhir);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ResponsiPemrograman3305{
+    class Program{
+        static void Main(string[] args){
+            // Deklarasi Variabel
+            Karyawan karyawan = new Karyawan();
+            karyawan.Nik = "123456789";
+            karyawan.Nama = "Sluwu";
+            karyawan.GajiBulanan = 3000000;
+
+            karyawan.infoKaryawan();
+            karyawan.naikGaji();
+        }
+    }
+}
